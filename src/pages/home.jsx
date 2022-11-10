7a256cd4528624ad5f681cfbb44e11aae0613161
t_heide <heide.lhe@link8tech.tw>
2022-11-10 18:20:44 +0800

    feat(首頁、sideBar):新增頁面

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import '../assets/css/style.scss'
+function Home(){
+    return(
+        <div className="home">
+            <div className='sideBar'>
+                <div className='sideBar__menu'></div>
+                <div className='sideBar__tab'>
+                    <div className='sideBar__tab__icon'></div>
+                </div>
+            </div>
+            <section className='section01'></section>
+        </div>
+    )
+}
+export default Home;
