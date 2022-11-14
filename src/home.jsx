c7c548ef1cb4471abdb7e02ca4c9a7ef12f5ecd7
t_heide <heide.lhe@link8tech.tw>
2022-11-14 18:13:07 +0800

    添加路徑地圖、站點顏色變數、鼠標樣式

--- src/home.jsx
+++ src/home.jsx
@@ -8,6 +8,8 @@ import ic_menu_list from './assets/img/ic/ic_menu_list.png'
 import ic_menu_list_h from './assets/img/ic/ic_menu_list_h.png'
 import ic_menu_strategy from './assets/img/ic/ic_menu_strategy.png'
 import ic_menu_strategy_h from './assets/img/ic/ic_menu_strategy_h.png'
+import map from './assets/img/main/map.svg';
+import map_now from './assets/img/main/map_now.gif'
 function Home(){
     // 側邊欄收合
     const [sideBar,setSideBar] = useState(false)
@@ -23,26 +25,26 @@ function Home(){
         <div className="home">
             <div className={`sideBar${sideBar?' active':''}`}>
                 <div className='sideBar__menu'>
-                    <div className='sideBar__menu__item'>
+                    <a className='sideBar__menu__item' href='https://2022.thef2e.com/news' target='_blank'>
                         <img className='img01' src={ic_menu_info} alt="" />
                         <img className='img02' src={ic_menu_info_h} alt="" />
                         <span>關卡資訊</span>
-                    </div>
-                    <div className='sideBar__menu__item'>
+                    </a>
+                    <a className='sideBar__menu__item' href='https://2022.thef2e.com/works' target='_blank'>
                         <img className='img01' src={ic_menu_list} alt="" />
                         <img className='img02' src={ic_menu_list_h} alt="" />
                         <span>作品列表</span>
-                    </div>
-                    <div className='sideBar__menu__item'>
+                    </a>
+                    <a className='sideBar__menu__item' href='https://hackmd.io/ofJD4K7iSI65V19zxC7d0w' target='_blank'>
                         <img className='img01' src={ic_menu_strategy} alt="" />
                         <img className='img02' src={ic_menu_strategy_h} alt="" />
                         <span>攻略資源</span>
-                    </div>
-                    <div className='sideBar__menu__item'>
+                    </a>
+                    <a className='sideBar__menu__item' href='https://2022.thef2e.com/jobs' target='_blank'>
                         <img className='img01' src={ic_menu_job} alt="" />
                         <img className='img02' src={ic_menu_job_h} alt="" />
                         <span>求職專區</span>
-                    </div>
+                    </a>
                 </div>
                 <div className='sideBar__tab'>
                     <div className={`sideBar__tab__icon ${mouseHandleIcon?'mouseOn':''}`}
@@ -51,6 +53,10 @@ function Home(){
                          onMouseLeave={() => setMouseHandleIcon(false)}
                     ></div>
                 </div>
+            </div>
+            <div className='map'>
+                <img src={map} alt="" />
+                <img src={map_now} className={`map__now`} alt="" />
             </div>
             <section className='section01'></section>
         </div>
